c98295837e5ef31ed528bff278ce071f2f999ec3
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-09-20 16:32:38 -0500

    Commit 1 Created Pet Class

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// 1.
+
+class pets {
+	constructor(name) {
+		this.name = name
+		this.owner = ""
+	}
+	setOwner(owner) {
+		this.owner = owner;
+	}
+}
+
+// const fluffy = new pets("fluffy");
+// fluffy.setOwner("john");
+// console.log(fluffy);
